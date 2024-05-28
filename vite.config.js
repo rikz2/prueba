e76f9ddd71f58971037eaafb3eda55b6d6278c53
rik <sergio.certificaciones.estudio@gmail.com>
2024-05-28 08:57:59 -0500

    deploy 4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://github.com/rikz2/prueba"
+  base:"https://rikz2.github.io/prueba/"
 })
